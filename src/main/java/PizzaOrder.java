d6fa92b2e8cf5a675ee01b3c587093a9c23eeb53
Julian Gutierrez <julian.milton.gutierrez@gmail.com>
2022-11-06 11:00:48 -0600

    made minor changes to the pizza order exercise

--- src/main/java/PizzaOrder.java
+++ src/main/java/PizzaOrder.java
@@ -17,15 +17,22 @@ public class PizzaOrder extends HttpServlet
     {
         request.getRequestDispatcher("/pizza-order.jsp").forward(request, response);
     }
-
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
         String crust = request.getParameter("crust");
         String sauce = request.getParameter("sauce");
         String size = request.getParameter("size");
-        String topping = request.getParameter("toppings");
+        String[] toppings = request.getParameterValues("toppings");
         String address = request.getParameter("address");
 
-        System.out.printf("Your picked crust as %s, sauce as %s, size is %s, the topping(s) is %s,and your address is %s.",crust,sauce,size,topping,address);
+        System.out.printf("You picked crust as %s, sauce as %s, size is %s, the topping(s) is ",crust,sauce,size);
+
+        for(String topping : toppings)
+        {
+            System.out.printf("%s, ",topping);
+        }
+
+        System.out.printf("and the address is %s.",address);
     }
 }
